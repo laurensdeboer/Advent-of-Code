78b0667357ce9dc6349898a2cdbc60346abc2bc6
Laurens de Boer <laurens.deboer220@gmail.com>
2022-12-01 22:19:05 +0100

    Day 1 part 2 finished

---
+++ 2022/Day 1/Part 2/main.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "main.h"
+
+int main()
+{
+    FILE *file;
+    file = fopen("../data.txt", "r");
+
+    if (file == NULL)
+        printf("Failed to open file...");
+
+    int mc = top_3_calories(file);
+    printf("Answer = %d\n", mc);
+
+    return 0;
+}
+
+/*
+ *
+ *  Function: top_3_calories(FILE *file)
+ *  -----------------------------------
+ *  Finds the elf carrying the most calories of food and returns this amount
+ * 
+ *  Parameter
+ *      file:   File with input data
+ * 
+ *  Returns
+ *      top3: Sum of the top three elves carrying the most calories
+ * 
+ */
+int top_3_calories(FILE *file)
+{
+    // Because the amount of elves is unknown, we'll just assume it's 1000 max
+    int arr[ARRAY_SIZE];
+
+    // Filling the array with -1 makes it easy to find where the real calorie data ends later on
+    for (int i = 0; i < ARRAY_SIZE; i++)
+        arr[i] = -1;
+
+    int calories = 0, idx = 0;
+    char line[MAX_LINE_LEN];
+
+    // Loops over each line in file, puts every elves calories in new array spot
+    while(fgets(line, MAX_LINE_LEN, file) != NULL)
+    {
+        int to_int = atoi(line);
+        if (to_int != 0)
+        {
+            calories += to_int;
+        }
+
+        else if (to_int == 0)
+        {
+            arr[idx] = calories;
+            calories = 0;
+            idx++;
+        }
+    }
+
+    // Fills the last array spot manually, because end of file is not a blank line
+    arr[idx] = calories;
+
+    int most_calories[TOP_THREE] = {0};
+
+    // Loops over the calorie amounts, saves the largest three calorie amounts
+    for (int i = 0; i < ARRAY_SIZE; i++)
+    {
+        if (arr[i] == -1)
+            break;
+        else
+        {
+            if (arr[i] > most_calories[PLACE_FIRST])
+            {
+                most_calories[PLACE_THIRD] = most_calories[PLACE_SECOND];
+                most_calories[PLACE_SECOND] = most_calories[PLACE_FIRST];
+                most_calories[PLACE_FIRST] = arr[i];
+            }
+            
+            else if (arr[i] > most_calories[PLACE_SECOND])
+            {
+                most_calories[PLACE_THIRD] = most_calories[PLACE_SECOND];
+                most_calories[PLACE_SECOND] = arr[i];
+            }
+
+            else if(arr[i] > most_calories[PLACE_THIRD])
+                most_calories[PLACE_THIRD] = arr[i];
+        }
+    }
+
+    // Add top 3 calorie amount together
+    int top_three = most_calories[PLACE_FIRST] + most_calories[PLACE_SECOND] + most_calories[PLACE_THIRD];
+    return top_three;
+}
